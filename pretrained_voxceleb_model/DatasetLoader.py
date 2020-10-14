Fixed additional speaker loss calculation related changes

pretrained_voxceleb_model/DatasetLoader.py
@@ -13,44 +13,89 @@ import math
 from scipy.io import wavfile
 from queue import Queue
 
-torchfb = torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_fft=2048, win_length=800, hop_length=200,
-                                                   window_fn=torch.hamming_window, n_mels=80)
+# torchfb = torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_fft=2048, win_length=800, hop_length=200,
+#                                                    window_fn=torch.hamming_window, n_mels=80)
+
 
 def round_down(num, divisor):
     return num - (num%divisor)
 
+
+def get_spectrogram_dctts(fpath):
+    import librosa
+    import numpy as np
+    y, sr = librosa.load(fpath, sr=16000)
+
+    # Trimming
+    y, _ = librosa.effects.trim(y)
+
+    # Preemphasis
+    y = np.append(y[0], y[1:] - 0.97 * y[:-1])
+
+    # stft
+    linear = librosa.stft(y=y,
+                          n_fft=2048,
+                          hop_length=200,
+                          win_length=800)
+
+    # magnitude spectrogram
+    mag = np.abs(linear)  # (1+n_fft//2, T)
+
+    # mel spectrogram
+    mel_basis = librosa.filters.mel(16000, 2048, 80)  # (n_mels, 1+n_fft//2)
+    mel = np.dot(mel_basis, mag)  # (n_mels, t)
+
+    # to decibel
+    mel = 20 * np.log10(np.maximum(1e-5, mel))
+    mag = 20 * np.log10(np.maximum(1e-5, mag))
+
+    max_db = 100
+    ref_db = 20
+    # normalize
+    mel = np.clip((mel - ref_db + max_db) / max_db, 1e-8, 1)
+    mag = np.clip((mag - ref_db + max_db) / max_db, 1e-8, 1)
+
+    # Transpose
+    mel = mel.T.astype(np.float32)  # (T, n_mels)
+    mag = mag.T.astype(np.float32)  # (T, 1+n_fft//2)
+    mel = mel[::4, :]
+    return torch.Tensor(mel)
+
+
 def loadWAV(filename, max_frames, evalmode=True, num_eval=10, n_mels=80):
 
-    # Maximum audio length
-    max_audio = max_frames * 160 + 240
+    # # Maximum audio length
+    # max_audio = max_frames * 160 + 240
+    # print("max_frames: ", max_frames)
+    # # Read wav file and convert to torch tensor
+    # sample_rate, audio  = wavfile.read(filename)
+    #
+    # audiosize = audio.shape[0]
+    #
+    # if audiosize <= max_audio:
+    #     shortage    = math.floor( ( max_audio - audiosize + 1 ) / 2 )
+    #     audio       = numpy.pad(audio, (shortage, shortage), 'constant', constant_values=0)
+    #     audiosize   = audio.shape[0]
+    #
+    # if evalmode:
+    #     startframe = numpy.linspace(0,audiosize-max_audio,num=num_eval)
+    # else:
+    #     startframe = numpy.array([numpy.int64(random.random()*(audiosize-max_audio))])
+    #
+    # feats = []
+    # if evalmode and max_frames == 0:
+    #     feats.append(audio)
+    # else:
+    #     for asf in startframe:
+    #         feats.append(audio[int(asf):int(asf)+max_audio])
+    #
+    # feat = numpy.stack(feats,axis=0)
+    #
+    # feat = torch.FloatTensor(feat)
 
-    # Read wav file and convert to torch tensor
-    sample_rate, audio  = wavfile.read(filename)
-
-    audiosize = audio.shape[0]
-
-    if audiosize <= max_audio:
-        shortage    = math.floor( ( max_audio - audiosize + 1 ) / 2 )
-        audio       = numpy.pad(audio, (shortage, shortage), 'constant', constant_values=0)
-        audiosize   = audio.shape[0]
-
-    if evalmode:
-        startframe = numpy.linspace(0,audiosize-max_audio,num=num_eval)
-    else:
-        startframe = numpy.array([numpy.int64(random.random()*(audiosize-max_audio))])
-    
-    feats = []
-    if evalmode and max_frames == 0:
-        feats.append(audio)
-    else:
-        for asf in startframe:
-            feats.append(audio[int(asf):int(asf)+max_audio])
-
-    feat = numpy.stack(feats,axis=0)
-
-    feat = torch.FloatTensor(feat)
-
-    mel_input = torchfb(feat) + 1e-6
+    mel_input = get_spectrogram_dctts(filename) + 1e-6
+    mel_input = mel_input.permute(1, 0)
+    mel_input = mel_input.unsqueeze(0)
 
     return mel_input
 

pretrained_voxceleb_model/SpeakerNet.py
@@ -39,18 +39,22 @@ def initialize_validation_file_list(listfilename):
 
 class SpeakerNet(nn.Module):
 
-    def __init__(self, lr = 0.0001, margin = 1, scale = 1, hard_rank = 0, hard_prob = 0, model="ResNetSE34L", nOut = 512, nSpeakers = 1000, optimizer = 'adam', encoder_type = 'SAP', normalize = True, trainfunc='softmax', n_mels=40, log_input=True, **kwargs):
+    # def __init__(self, lr = 0.0001, margin = 1, scale = 1, hard_rank = 0, hard_prob = 0, model="ResNetSE34L", nOut = 512, nSpeakers = 1000, optimizer = 'adam', encoder_type = 'SAP', normalize = True, trainfunc='softmax', n_mels=40, log_input=True, **kwargs):
+    def __init__(self, lr=0.001, margin=0.3, scale=30, hard_rank=10, hard_prob=0.5, model="ResNetSE34L", nOut=512,
+                     nSpeakers=18, optimizer='adam', encoder_type='SAP', normalize=True, trainfunc='softmax',
+                     n_mels=80, log_input=True, **kwargs):
         super(SpeakerNet, self).__init__();
 
         argsdict = {'nOut': nOut, 'encoder_type':encoder_type, 'nClasses':nSpeakers, 'margin':margin, 'scale':scale, 'hard_prob':hard_prob, 'hard_rank':hard_rank, 'log_input':log_input, 'n_mels':n_mels}
+        print(argsdict)
 
         SpeakerNetModel = importlib.import_module('pretrained_voxceleb_model.'+model).__getattribute__('MainModel')
-        self.__S__ = SpeakerNetModel(**argsdict).cuda();
+        self.__S__ = SpeakerNetModel(**argsdict).cuda()
 
         if optimizer == 'adam':
-            self.__optimizer__ = torch.optim.Adam(self.parameters(), lr = lr);
+            self.__optimizer__ = torch.optim.Adam(self.parameters(), lr = lr)
         elif optimizer == 'sgd':
-            self.__optimizer__ = torch.optim.SGD(self.parameters(), lr = lr, momentum = 0.9, weight_decay=5e-5);
+            self.__optimizer__ = torch.optim.SGD(self.parameters(), lr = lr, momentum = 0.9, weight_decay=5e-5)
         else:
             raise ValueError('Undefined optimizer.')
 
@@ -88,19 +92,22 @@ class SpeakerNet(nn.Module):
     # evaluate Y generated from the dc tts network
     def evaluateY(self, Y, speakers):
 
+        self.eval()
         feats = {}
         batch_length = Y.shape[0]
         lines = []
 
         for i in range(batch_length):
             speaker = speakers[i]
-            feats['Y'+str(i)] = Y[i]
+            feats['Y'+str(i)] = Y[i] # [:,:,:40]
 
             # same speakers
             speaker_item = speaker.item()
             comp_file_path = random.choices(self.test_files[speaker_item], k=hp.same_spk_samples)
             for j in range(len(comp_file_path)):
+                # print("file: ", comp_file_path[j])
                 feats[comp_file_path[j]] = numpy.load(hp.embedding_files + comp_file_path[j] + ".npy")
+                # print(feats[comp_file_path[j]].shape)
                 lines.append("1 " + "Y" + str(i) + " " + comp_file_path[j] + "\n")
 
             # other speakers
@@ -111,8 +118,16 @@ class SpeakerNet(nn.Module):
                 comp_file_path.extend(random.choices(self.test_files[spk], k=hp.other_spk_samples))
 
             for j in range(len(comp_file_path)):
+                # print("file: ", comp_file_path[j])
                 feats[comp_file_path[j]] = numpy.load(hp.embedding_files + comp_file_path[j] + ".npy")
+                # print(feats[comp_file_path[j]].shape)
                 lines.append("0 " + "Y" + str(i) + " " + comp_file_path[j] + "\n")
+
+        # lines = ['1 Y0 bas_rnd3_127', '1 Y0 bas_rnd2_371', '0 Y0 bea_rnd3_166', '0 Y0 cau_rnd2_038', '0 Y0 dcs_rnd3_031', '0 Y0 ddm_rnd2_335', '0 Y0 eme_rnd2_457', '0 Y0 fds_rnd1_014', '0 Y0 htm_rnd1_067', '0 Y0 ips_rnd2_184', '0 Y0 maria_rnd2_301', '0 Y0 pcs_rnd2_108', '0 Y0 pmm_rnd1_274', '0 Y0 pss_rnd1_044', '0 Y0 rms_rnd1_256', '0 Y0 sam_rnd2_365', '0 Y0 sds_rnd1_228', '0 Y0 sgs_rnd1_238', '0 Y0 tim_rnd2_381', '0 Y0 tss_rnd1_433']
+        # lines = ['1 Y0 fds_rnd2_432', '1 Y0 fds_rnd1_257', '0 Y0 bas_rnd2_400', '0 Y0 bea_rnd2_142', '0 Y0 cau_rnd2_120', '0 Y0 dcs_rnd3_431', '0 Y0 ddm_rnd1_090', '0 Y0 eme_rnd3_228', '0 Y0 htm_rnd1_204', '0 Y0 ips_rnd2_366', '0 Y0 maria_rnd1_018', '0 Y0 pcs_rnd2_417', '0 Y0 pmm_rnd1_353', '0 Y0 pss_rnd2_093', '0 Y0 rms_rnd2_383', '0 Y0 sam_rnd1_212', '0 Y0 sds_rnd2_199', '0 Y0 sgs_rnd2_467', '0 Y0 tim_rnd2_287', '0 Y0 tss_rnd2_371', '1 Y1 sam_rnd2_149', '1 Y1 sam_rnd2_125', '0 Y1 bas_rnd3_443', '0 Y1 bea_rnd1_446', '0 Y1 cau_rnd2_043', '0 Y1 dcs_rnd1_223', '0 Y1 ddm_rnd1_120', '0 Y1 eme_rnd3_062', '0 Y1 fds_rnd2_378', '0 Y1 htm_rnd2_317', '0 Y1 ips_rnd2_172', '0 Y1 maria_rnd1_305', '0 Y1 pcs_rnd1_478', '0 Y1 pmm_rnd1_140', '0 Y1 pss_rnd3_297', '0 Y1 rms_rnd2_461', '0 Y1 sds_rnd2_090', '0 Y1 sgs_rnd1_487', '0 Y1 tim_rnd1_379', '0 Y1 tss_rnd1_202']
+        # for i in range(len(lines)):
+        #     l, _, f = lines[i].split(" ")
+        #     feats[f] = numpy.load(hp.embedding_files + f + ".npy")
 
         # print(lines)
 
@@ -123,7 +138,7 @@ class SpeakerNet(nn.Module):
         # Read files and compute all scores
         for idx, line in enumerate(lines):
 
-            data = line.split();
+            data = line.split()
 
             ## Append random label if missing
             if len(data) == 2: data = [random.randint(0, 1)] + data
@@ -158,7 +173,7 @@ class SpeakerNet(nn.Module):
             param_group['lr'] = param_group['lr']*alpha
             learning_rate.append(param_group['lr'])
 
-        return learning_rate;
+        return learning_rate
 
 
     ## ===== ===== ===== ===== ===== ===== ===== =====
@@ -169,7 +184,6 @@ class SpeakerNet(nn.Module):
         
         torch.save(self.state_dict(), path);
 
-
     ## ===== ===== ===== ===== ===== ===== ===== =====
     ## Load parameters
     ## ===== ===== ===== ===== ===== ===== ===== =====
@@ -178,6 +192,7 @@ class SpeakerNet(nn.Module):
 
         self_state = self.state_dict()
         loaded_state = torch.load(path)
+        print("Loading parameters from: ", path)
         for name, param in loaded_state.items():
             origname = name;
             if name not in self_state:

synthesize.py
@@ -20,7 +20,7 @@ parser.add_argument("--dataset", required=True, choices=['ljspeech', 'mbspeech',
 args = parser.parse_args()
 speaker2ix = dict(zip(hp.speaker_list, range(len(hp.speaker_list))))
 
-os.environ["CUDA_VISIBLE_DEVICES"]="3"
+# os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
 if args.dataset == 'ljspeech':
     from datasets.lj_speech import vocab, get_test_data
@@ -53,11 +53,26 @@ elif args.dataset == 'swara':
     spk = ['BAS', 'BEA', 'CAU', 'DCS', 'DDM', 'EME', 'FDS', 'HTM', 'IPS', 'MARIA', 'PCS',
      'PMM', 'PSS', 'RMS', 'SAM', 'SDS', 'SGS', 'TSS']
     SENTENCES = [
-        "Ana are mere.|"+sp for sp in spk]
+        "Se așteaptă ca acest mânz să fie unul dintre cele mai importante exemplare din rasa sa, din punct de vedere genetic.|"+sp for sp in spk]
+        # "La automatele de vândut cartele se poate achita cu numerar.|"+sp for sp in spk]
         # "Ana are mere.|EME",
-        # "de asemenea contează și dacă imobilul este la stradă sau nu.|DCS",
+        # "de asemenea contează și dacă imobilul este la stradă sau nu.|MARIA",
+        # "de aceea spune medicul autoritățile ar trebui să schimbe tonul discuțiilor despre coronavirus.|BAS",
+        # "Diaconu a explicat că a fi asimptomatic este o raritate în orice boală, dar că să mărturisești că ai simptome poate duce la stigmă socială, de aceea mulți oameni preferă să tacă și să ascundă ceea ce li se întâmplă.|SAM",
+        # "Suntem pregătiți să dăm în trafic cu călători Magistrala de metroul Drumul Taberei, Eroilor în cursul zilei de mâine|BEA"
+        # "După semnarea celor două procese verbale de recepție la terminarea lucrărilor, respectiv la punerea în funcțiune, pe data de unu septembrie, am anunțat public",
+        # "După îndeplinirea tuturor procedurilor administrative, care au mai rămas de efectuat de către beneficiar",
+        # "O altă activitate necesară constă în predarea obiectivelor de pază pe staţii şi interstaţii",
+        # "Astfel, în aceasta perioadă, operatorii de telefonie mobilă au finalizat instalarea reţelelor de comunicaţii în subteran.",
+        # "La automatele de vândut cartele se poate achita cu numerar.",
+        # "Achitarea cu card bancar urmează să se implementeze cât mai curând posibil împreună cu banca suport.",
+        # "Este necesar a se realiza o soluţie de comunicaţie stabilă care presupune atât configurarea cât şi instalarea unor echipamente şi chei de criptare.",
+        # "De asemenea se predau toate spaţiile publice de la firmele de curăţenie ale constructorului.",
+        # "Au semnat procesele-verbale în urma cărora această magistrală de metrou poate fi dată în exploatare şi, totodată, în trafic cu călători.",
+        # "Tehnologiile avansate de reproducere, inclusiv clonarea, pot salva speciile, permițându-ne să restabilim diversitatea genetică ce altfel s-ar fi pierdut în timp",
+        # "Când va crește, Kurt va fi mutat în Parcul Safari de la grădina zoologică, pentru reproducere.",
         # "Se așteaptă ca acest mânz să fie unul dintre cele mai importante exemplare din rasa sa, din punct de vedere genetic."
-    #]
+    # ]
 elif args.dataset == 'swara_test':
     from datasets.swara_test import vocab, get_test_data
 
@@ -65,11 +80,6 @@ elif args.dataset == 'swara_test':
            'PMM', 'PSS', 'RMS', 'SAM', 'SDS', 'SGS', 'TSS']
     SENTENCES = [
         "ana are mere și pere.|" + sp for sp in spk]
-        # "ana are mere.|SAM",
-        # "cercetătorii spun că persoanele care nu au vrut să recunoască simptomele s-ar fi temut.|IPS",
-        # "din pacienții covid au mințit că nu au simptome care nu au vrut să recunoască simptomele.|SAM",
-        # "De asemenea contează și dacă imobilul este la stradă sau nu.|FDS",
-    #     "de aceea spune medicul autoritățile ar trebui să schimbe tonul discuțiilor despre coronavirus.|PSS"  # ,
     # ]
 elif args.dataset == 'mbspeech':
     from datasets.mb_speech import vocab, get_test_data
@@ -155,4 +165,15 @@ for i in range(len(SENTENCES)):
     save_to_png('samples/%d-att.png' % (i + 1), A[0, :, :])
     save_to_png('samples/%d-mel.png' % (i + 1), Y[0, :, :])
     save_to_png('samples/%d-mag.png' % (i + 1), Z[0, :, :])
-    save_to_wav(Z[0, :, :].T, 'samples/%d-wav.wav' % (i + 1))
+    # import matplotlib.pyplot as plt
+    # a = self.embeddings(torch.tensor([x for x in range(10)]).cuda())
+    # fig, ax = plt.subplots()
+    # plt.imshow(A[0,:,:23])
+    # labels = [item.get_text() for item in ax.get_xticklabels()]
+    # labels=[x for x in sentence]
+    #
+    # ax.set_yticklabels(labels[::-1])
+    # plt.show()
+    # save_to_wav(Z[0, :, :].T, 'samples/%d-wav.wav' % (i + 1))
+    print('saving for speaker: ', speaker)
+    save_to_wav(Z[0, :, :].T, 'samples/%d-%s-wav.wav' % ((i + 1), speaker))

train-text2mel.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 """Train the Text2Mel network. See: https://arxiv.org/abs/1710.08969"""
 __author__ = 'Erdene-Ochir Tuguldur'
-
+import matplotlib.pylab as pl
+import numpy as np
 import sys, os
 import time
 import argparse
@@ -44,10 +45,12 @@ print('use_gpu', use_gpu)
 if use_gpu:
     torch.backends.cudnn.benchmark = True
 
-train_data_loader = Text2MelDataLoader(text2mel_dataset=SpeechDataset(['texts', 'mels', 'mel_gates', 'speakers']),
-                                       batch_size=32, mode='train')
-valid_data_loader = Text2MelDataLoader(text2mel_dataset=SpeechDataset(['texts', 'mels', 'mel_gates', 'speakers']),
-                                       batch_size=32, mode='valid')
+train_data_loader = Text2MelDataLoader(text2mel_dataset=SpeechDataset(['texts', 'mels', 'mel_gates', 'speakers',
+                                                                       'filenames']),
+                                       batch_size=16, mode='train')
+valid_data_loader = Text2MelDataLoader(text2mel_dataset=SpeechDataset(['texts', 'mels', 'mel_gates', 'speakers',
+                                                                       'filenames']),
+                                       batch_size=16, mode='valid')
 
 text2mel = Text2Mel(vocab).cuda()
 
@@ -72,8 +75,12 @@ if last_checkpoint_file_name:
 if hp.use_additional_speaker_loss:
     s = SpeakerNet()
     s.loadParameters(hp.speaker_recognition_model_path)
-    s.eval()
+    # for name, param in s.named_parameters():
+    #     if param.requires_grad:
+    #         print(name, param.data)
+
     s = s.cuda()
+    s.eval()
     f = open(logger.logdir + "/eer_log.txt", "a")
 
 
@@ -102,9 +109,12 @@ def train(train_epoch, phase='train'):
     running_att_loss = 0.0
     running_eer_loss = 0.0
 
+    cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+
     pbar = tqdm(data_loader, unit="audios", unit_scale=data_loader.batch_size, disable=hp.disable_progress_bar)
     for batch in pbar:
-        L, S, gates, speakers = batch['texts'], batch['mels'], batch['mel_gates'], batch['speakers']
+        L, S, gates, speakers, filenames = batch['texts'], batch['mels'], batch['mel_gates'], batch['speakers'], batch['filenames']
+        # print(filenames)
         S = S.permute(0, 2, 1)  # TODO: because of pre processing
 
         B, N = L.size()  # batch size and text count
@@ -138,17 +148,33 @@ def train(train_epoch, phase='train'):
         masks = gates.reshape(B, 1, T).float()
         att_loss = (A * W * masks).mean()
 
+        eer_loss = 0
         if hp.use_additional_speaker_loss:
-            y_embedding = s.__S__.forward(Y)
-            sc, lab, _ = s.evaluateY(y_embedding, speakers)
-            # print("scores: ", sc)
-            result = tuneThresholdfromScore(sc, lab, [1, 0.1])
-            eer_loss = result[1]/100
+            s.eval()
+            for i in range(B):
+                index = [j for j in range(Y.shape[2]) if gates[i, j] == 1][-1]
+                y_embedding = s.__S__.forward(Y[i, :, :index].unsqueeze(0))
+                if hp.speaker_loss_type == 'Equal_Error_Rate':
+                    sc, lab, _ = s.evaluateY(y_embedding, speakers)
+                    result = tuneThresholdfromScore(sc, lab, [1, 0.1])
+                    eer_loss = result[1] / 100
+                elif hp.speaker_loss_type == 'Cosine_Similarity':
+                    y_nat = s.__S__.forward(S[i, :, :index].unsqueeze(0))
+                    eer_loss += cos(y_embedding, y_nat)
+            eer_loss = eer_loss / B
+
             f.write("train_epoch: %d, it: %d, eer_loss: %f \n" % (train_epoch, it, eer_loss))
         else:
             eer_loss = 0
 
-        loss = l1_loss + att_loss + eer_loss
+        if hp.use_additional_speaker_loss:
+            if hp.speaker_loss_type == 'Equal_Error_Rate':
+                l1_loss.data = torch.Tensor([eer_loss]).cuda()
+                loss = l1_loss
+            elif hp.speaker_loss_type == 'Cosine_Similarity':
+                loss = -eer_loss
+        else:
+            loss = l1_loss + att_loss
 
         if phase == 'train':
             lr_decay(global_step)
@@ -176,13 +202,16 @@ def train(train_epoch, phase='train'):
             pbar.set_postfix({
                 'l1': "%.05f" % (running_l1_loss / it),
                 'att': "%.05f" % (running_att_loss / it),
-                'eer': "%.05f" % (running_eer_loss / it)
+                'eer_running': "%.05f" % (running_eer_loss / it),
+                'eer' : "%.05f" % (eer_loss)
             })
             logger.log_step(phase, global_step, {'loss_l1': l1_loss, 'loss_att': att_loss, 'loss_eer': eer_loss},
                             {'mels-true': S[:1, :, :], 'mels-pred': Y[:1, :, :], 'attention': A[:1, :, :]})
             if global_step % 5000 == 0:
                 # checkpoint at every 5000th step
                 save_checkpoint(logger.logdir, train_epoch, global_step, text2mel, optimizer)
+
+        # sys.exit(0)
 
     epoch_loss = running_loss / it
     epoch_l1_loss = running_l1_loss / it
@@ -207,8 +236,8 @@ while True:
                                                                      time_elapsed % 60)
     print("train epoch loss %f, step=%d, %s" % (train_epoch_loss, global_step, time_str))
 
-    valid_epoch_loss = train(epoch, phase='valid')
-    print("valid epoch loss %f" % valid_epoch_loss)
+    #valid_epoch_loss = train(epoch, phase='valid')
+    #print("valid epoch loss %f" % valid_epoch_loss)
 
     epoch += 1
     if global_step >= hp.text2mel_max_iteration: